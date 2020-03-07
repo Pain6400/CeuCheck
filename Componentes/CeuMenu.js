ce7adfae1aaa59f6e0ddbef04d08ed7033609432
kevinMejia <59616614+KevinMejia6400@users.noreply.github.com>
2020-03-07 13:26:19 -0600

    Estilo de menu

--- Componentes/CeuMenu.js
+++ Componentes/CeuMenu.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { StyleSheet, FlatList, Text, View } from 'react-native';
+import { StyleSheet, FlatList, Text, View, Button } from 'react-native';
 import { CeuModules } from '../Data/CeuModules';
 
 const CeuMenu = props =>{
     return (
         <View style={style.screen}>
             <View style={style.evaluacion}> 
-                <Text>Test</Text>
+                <Text style={style.title} numberOfLines={2}>Evaluacion al docente</Text>
             </View>
+            <Text></Text>
             <View style={style.asistencia}>
-
+                <Text style={style.title} numberOfLines={2}>Asistencia del docente</Text>
             </View>
+            <Text></Text>
             <View style={style.reportes}> 
-
+                <Text style={style.title} numberOfLines={2}>Reportes</Text>
             </View>
-            <View>
+            <View style={style.fooder}>
                 
             </View>
         </View>
@@ -32,19 +34,36 @@ const style = StyleSheet.create({
         backgroundColor: 'white'
     },
     evaluacion:{
-        flex: 1,
+        flex: 0.5,
+        borderRadius: 400,
+        borderWidth: 5,
+        borderColor: 'black',
         justifyContent: 'center',
         alignItems: 'center'
     },
     asistencia: {
-        flex: 1,
-        margin: 15,
-        height: 150
+        flex: 0.5,
+        borderRadius: 400,
+        borderWidth: 5,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     reportes: {
+        flex: 0.5,
+        borderRadius: 400,
+        borderWidth: 5,
+        borderColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fooder:{
         flex: 1,
-        margin: 15,
-        height: 150
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'right'
     }
 });
 
