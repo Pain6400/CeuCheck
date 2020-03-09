bbfe722d8ff6a2ad488a096f36e32816ff5b58a6
kevinMejia <59616614+KevinMejia6400@users.noreply.github.com>
2020-03-09 08:32:14 -0600

    Formulario

--- Componentes/Admin/EvaluacionDocente.js
+++ Componentes/Admin/EvaluacionDocente.js
@@ -5,36 +5,60 @@ export default class Evaluacion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 50,
+      question1: 0,
+      question2: 0,
     };
   }
 
-  change(value) {
+  changeQuestion1(value) {
     this.setState(() => {
       return {
-        value: parseFloat(value),
+        question1: parseFloat(value),
+      };
+    });
+  }
+
+  changeQuestion2(value) {
+    this.setState(() => {
+      return {
+        question2: parseFloat(value),
       };
     });
   }
 
   render() {
-    const {value} = this.state;
+    const {question1, question2} = this.state;
     return (
-      <View>
-          <Text>El docente asiste puntualmente a clases?</Text>
+      <View style={styles.screen}>
+        <Text></Text>
+          <Text>El profesor asiste puntualmente a sus clases?</Text>
         <Slider
           step={1}
           maximumValue={5}
-          onValueChange={this.change.bind(this)}
-          value={value}
+          onValueChange={this.changeQuestion1.bind(this)}
+          value={question1}
         />
-        <Text style={styles.text}>{String(value)}</Text>
+        <Text style={styles.text}>{String(question1)}</Text>
+
+        <Text>El profesor presenta los objetivos al inicio de la clase?</Text>
+        <Slider
+          step={1}
+          maximumValue={5}
+          onValueChange={this.changeQuestion2.bind(this)}
+          value={question2}
+        />
+        <Text style={styles.text}>{String(question2)}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    flexDirection : 'column',
+    backgroundColor: 'white'
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
