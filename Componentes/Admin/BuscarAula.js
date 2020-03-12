27f062407884027d5f832c1ec42f627d4b7b8f02
kevinMejia <59616614+KevinMejia6400@users.noreply.github.com>
2020-03-12 16:57:45 -0600

    cambios

--- Componentes/Admin/BuscarAula.js
+++ Componentes/Admin/BuscarAula.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 
+const lista = [{ key: 'test' },
+{ key: 'test2' },
+{ key: 'Dominic' },
+{ key: 'Jackson' },
+{ key: 'James' },
+{ key: 'Joel' },
+{ key: 'John' },
+{ key: 'Jillian' },
+{ key: 'Jimmy' },
+{ key: 'Julie' }]
+
+
+
 const BuscarAula = props => {
+    const [list, CargarListApi] = useState(lista)
+    const cargarLista = () => {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data) => {
+            let nuevaLista = [];
+            data.map(item => {
+                nuevaLista.push({key: item.name})
+            })
+          CargarListApi(nuevaLista)
+        })
+        .catch(console.log)
+    }
+
     return (
         <View style={styles.container}>
              <FlatList
-          data={[
-            {key: 'test'},
-            {key: 'Dan'},
-            {key: 'Dominic'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-          ]}
+          data={list}
           renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
         />
+        <Button title="CargarLista" onPress={cargarLista} />
         </View>
     );
 }
